7ea0590df0cf4ddf956a99638fb7d9c748ddd786
= <=>
2018-07-02 17:35:26 -0600

    returning both in json base64 strings, the html is just a string however not a file. Yet

--- app/puppeteer/crawler.js
+++ app/puppeteer/crawler.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
 
-
 async function pageScreenshot(url, cb) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -13,11 +12,21 @@ async function pageHTML(url, cb) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
-  let html = await page.evaluate(() => document.body.innerHTML);
+  let html = await page.content();
   await browser.close();
   cb(html);
 }
+async function pageScreenshotAndHTML(url,cb) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  let screenshot = await page.screenshot({fullPage: true});
+  let html = await page.content();
+  await browser.close();
+  cb(screenshot,html);
+}
 module.exports = {
   pageScreenshot: pageScreenshot,
-  pageHTML: pageHTML
+  pageHTML: pageHTML,
+  pageScreenshotAndHTML: pageScreenshotAndHTML
 }
--- app/routes/crawler_routes.js
+++ app/routes/crawler_routes.js
@@ -4,14 +4,29 @@ module.exports = function(app) {
   app.get('/crawl/screenshot/:url', (req, res) => {
     const url = req.params.url;
     crawler.pageScreenshot(url, function(screenshot) {
-      res.contentType('image/png');
-      res.send(screenshot);
+      let buffer = new Buffer(screenshot);
+      let myScreenshot = buffer.toString('base64');
+      // res.contentType('image/png');
+      res.send(myScreenshot);
     });
   });
   app.get('/crawl/html/:url', (req, res) => {
     const url = req.params.url;
     crawler.pageHTML(url, function(html) {
-      res.send(html);
+      console.log(typeof html);
+      let buffer = Buffer.from(html).toString('base64');
+      // res.contentType('text/html');
+      res.send(buffer);
+    });
+  });
+  app.get('/crawl/screenshot_and_html/:url', (req, res) => {
+    const url = req.params.url;
+    crawler.pageScreenshotAndHTML(url, function(screenshot,html) {
+      let buffedScreen = Buffer.from(screenshot).toString('base64');
+      let buffedHtml = Buffer.from(html).toString('base64');
+      let obj = JSON.stringify({'screenshot': buffedScreen, 'html': buffedHtml});
+      res.contentType('application/json');
+      res.send(obj);
     });
   });
 };
