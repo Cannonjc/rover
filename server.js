7f02f3785b6d7afc4528b574de4d9d85d656d24d
= <=>
2018-07-11 13:49:47 -0600

    removed comment

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser  = require('body-parser');
 
 const app = express();
 
-// const port = 8000;
 const port = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
