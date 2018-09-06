85d2ce5607575af8231998ed08912175cdca002e
= <=>
2018-09-06 10:30:11 -0600

    new crawler stuff

--- app/puppeteer/crawler.js
+++ app/puppeteer/crawler.js
@@ -2,8 +2,12 @@ const puppeteer = require('puppeteer');
 const xPathToCss = require('xpath-to-css');
 const fs = require('fs');
 const mkdirp = require('mkdirp');
+const axios = require('axios');
+
+let recipe = {};
 let storage = {};
 let recordCount = 1;
+let initData = {};
 
 async function pageScreenshot(url, cb) {
   const browser = await puppeteer.launch();
@@ -31,12 +35,23 @@ async function pageScreenshotAndHTML(url,cb) {
   cb(screenshot,html);
 };
 
-async function mainCrawl(params) {
-  const browser = await puppeteer.launch({headless: true});
+async function mainCrawl(params, cb) {
+  recipe = params;
+  if (checkInitData(params['initialize'])) {
+    cb("");
+  } else {
+    cb("Initialize data is not complete");
+    return;
+  }
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-
-  for (var key in params) {
-    await decider(page, key, params);
+  for (var key in recipe) {
+    if (recipe.error) {
+      sendError(recipe.error);
+      break;
+    } else {
+      await decider(page, key, recipe);
+    }
   }
   // await page.waitFor(4000);
   await browser.close();
@@ -46,6 +61,9 @@ async function decider(page, key, params) {
   console.log(key);
   // console.log(params[key]);
   switch(key) {
+    case 'initialize':
+      await initialize(params[key])
+      break;
     case 'start_url':
       await startUrl(page, params[key]);
       break;
@@ -61,12 +79,15 @@ async function decider(page, key, params) {
     case 'record_links':
       await traverseLinks(page, params[key]);
       break;
-    case 'save_and_erase':
-      await save_and_erase();
-      break;
     case 'click':
-      await page.click(xPathToCss(params[key]));
+      await page.click(convertXPath(params[key], setErrorInRecipe));
       break;
+    // case 'save_and_erase':
+    //   await save_and_erase();
+    //   break;
+    // case 'send_record':
+    //   sendRecord(storage['record_screenshot'],storage['records_html'],storage['details_screenshot'],storage['details_html']);
+    //   break;
     // case 'record_detail_links':
       // await traverseLinks();
       // break;
@@ -75,24 +96,42 @@ async function decider(page, key, params) {
   }
 };
 
+function checkInitData(initialize) {
+  if (initialize['respond_url'] && initialize['error_url'] && initialize['crawl_id']) {
+    return true;
+  } else {
+    return false;
+  }
+}
+async function initialize(params) {
+  if (params['respond_url'] && params['error_url'] && params['crawl_id']) {
+    initData.respondUrl = params['respond_url'];
+    initData.errorUrl = params['error_url'];
+    initData.crawlID = params['crawl_id'];
+  } else {
+    await setErrorInRecipe("Initialize data is not complete");
+  }
+}
+
 async function startUrl(page, url) {
   await page.goto(url);
 };
 
 async function traverseLinks(page, linksInfo) {
   console.log('linksInfo');
-  console.log(linksInfo);
+  // console.log(linksInfo);
   // console.log(JSON.parse(linksInfo)['links_selector']);
-  // let json = JSON.parse(linksInfo)
   let links = await getLinksFromXPathSelector(page, linksInfo['links_selector']);
   // console.log(links);
   for (i = 0; i < links.length; i++) {
   // for (var link in links) {
-    await goToRecordPage(page, links[i], linksInfo['record']);
+    await goToDetailsPage(page, links[i], linksInfo['record']);
     // functionality of what to do with each record to change
+    // We need the index for each link, so we need to just send the record after each time it goes to the details page
+    sendRecord(i,storage['records_screenshot'],storage['records_html'],storage['details_screenshot'],storage['details_html']);
   }
 };
-async function goToRecordPage(page,link, linkInfo) {
+async function goToDetailsPage(page,link, linkInfo) {
   console.log('going to link: ', link);
   await Promise.all([
     page.waitForNavigation(),
@@ -109,10 +148,10 @@ async function goToRecordPage(page,link, linkInfo) {
 
 async function fillForm(page,keyValues) {
   console.log('filling form');
-  console.log(keyValues);
+  // console.log(keyValues);
   for (var key in keyValues) {
-    console.log(key);
-    console.log(keyValues[key]);
+    // console.log(key);
+    // console.log(keyValues[key]);
     switch(key) {
       case 'text_field':
         await textFieldFill(page, keyValues[key]);
@@ -121,7 +160,7 @@ async function fillForm(page,keyValues) {
         await selectFill(page, keyValues[key]);
         break;
       case 'submit':
-        await page.click(xPathToCss(keyValues[key]));
+        await page.click(convertXPath(keyValues[key],setErrorInRecipe));
         break;
       default:
         break;
@@ -131,35 +170,76 @@ async function fillForm(page,keyValues) {
 async function textFieldFill(page, selectorsValues) {
   console.log('Filling text fields');
   for (var selector in selectorsValues) {
-    console.log(selector);
-    console.log(xPathToCss(selector));
-    console.log(selectorsValues[selector]);
-    await page.click(xPathToCss(selector));
-    console.log('test');
+    // console.log(selector);
+    console.log(convertXPath(selector,setErrorInRecipe));
+    // console.log(selectorsValues[selector]);
+    await page.click(convertXPath(selector,setErrorInRecipe));
+    // console.log('test');
     await page.keyboard.type(selectorsValues[selector]);
   }
 };
 async function selectFill(page, selectorsValues) {
-  // console.log('selectors fields');
+  console.log('selectors fields');
   for (var selector in selectorsValues) {
-    await page.select(xPathToCss(selector), selectorsValues[selector]);
+    await page.select(convertXPath(selector,setErrorInRecipe), selectorsValues[selector]);
   }
 };
-
 
 async function save_and_erase() {
   let path = await 'screenshots/item'+recordCount;
   await makeDirectory(path);
   await makeDirectory(path+'/base');
   await makeDirectory(path+'/record');
-  await saveFile(path+'/base/base.html',storage['index_html']);
-  await saveFile(path+'/base/base.png',storage['index_screenshot']);
-  await saveFile(path+'/record/record.html',storage['record_html']);
-  await saveFile(path+'/record/record.png',storage['record_screenshot']);
+  await saveFile(path+'/base/base.html',storage['records_html']);
+  await saveFile(path+'/base/base.png',storage['records_screenshot']);
+  await saveFile(path+'/record/record.html',storage['details_html']);
+  await saveFile(path+'/record/record.png',storage['details_screenshot']);
   delete storage['record_html'];
   delete storage['record_screenshot'];
   await recordCount++;
 };
+async function sendRecord(index,recordsScreenshot, recordsHtml, detailsScreenshot, detailsHtml) {
+  console.log("sending");
+  console.log(initData.respondUrl);
+  let buffedRS = Buffer.from(recordsScreenshot).toString('base64');
+  let buffedRH = Buffer.from(recordsHtml).toString('base64');
+  let buffedDS = Buffer.from(detailsScreenshot).toString('base64');
+  let buffedDH = Buffer.from(detailsHtml).toString('base64');
+  // let breeds = await axios.get('http://localhost:3003/api/v1/crawls')
+  // console.log(breeds);
+  // axios({
+  //   method: 'post',
+  //   url: initData.respondUrl,
+  //   data: {
+  //     crawl_id: initData.crawlID
+  //   }
+  // }).catch(function (error) {
+  //   // handle error
+  //   console.log(error);
+  // });
+  axios({
+    method: 'post',
+    url: initData.respondUrl,
+    // url: "/api/v1/crawls/rover_page",
+    // proxy: {
+    //   host: '127.0.0.1',
+    //   port: 3003
+    // },
+    data: {
+      crawl_id: initData.crawlID,
+      link_num: index,
+      records_screenshot: buffedRS,
+      records_html: buffedRH,
+      details_screenshot: buffedDS,
+      details_html: buffedDH
+    }
+  }).catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+};
+
+
 // async function makeDirectory(path) {
 // 	mkdirp(path, function(err) {
 // 		if (err) throw err;
@@ -186,10 +266,38 @@ function saveFile(path, content) {
         });
     })
 };
+function convertXPath(xpath, cb) {
+  try {
+    let myConversion = xPathToCss(xpath)
+    return myConversion;
+  } catch(e) {
+    cb(e)
+  }
+}
+
+function setErrorInRecipe(e) {
+  recipe.error = JSON.stringify(e);
+}
+
+async function sendError(e) {
+  axios({
+    method: 'post',
+    url: initData.errorUrl,
+    data: {
+      error: e
+    }
+  }).then((response) => {
+
+  }).catch((erorr) => {
+    console.log("We hit an error with sending the error: ",error.message)
+  });
+}
+
+
 
 async function getLinksFromXPathSelector(page,selector) {
   console.log('links selector');
-  console.log(selector);
+  // console.log(selector);
   const links = await page.evaluate((mySelector) => {
 		let results = [];
   	let query = document.evaluate(mySelector,
--- app/routes/crawler_routes.js
+++ app/routes/crawler_routes.js
@@ -3,21 +3,21 @@ const crawler = require('./../puppeteer/crawler.js');
 module.exports = function(app) {
   app.get('/crawl/screenshot/:url', (req, res) => {
     const url = req.params.url;
-    crawler.pageScreenshot(url, function(screenshot) {
+    crawler.pageScreenshot(url, (screenshot) => {
       res.contentType('image/png');
       res.send(screenshot);
     });
   });
   app.get('/crawl/html/:url', (req, res) => {
     const url = req.params.url;
-    crawler.pageHTML(url, function(html) {
+    crawler.pageHTML(url, (html) => {
       res.contentType('text/html');
       res.send(html);
     });
   });
   app.get('/crawl/screenshot_and_html/:url', (req, res) => {
     const url = req.params.url;
-    crawler.pageScreenshotAndHTML(url, function(screenshot,html) {
+    crawler.pageScreenshotAndHTML(url, (screenshot,html) => {
       let buffedScreen = Buffer.from(screenshot).toString('base64');
       let buffedHtml = Buffer.from(html).toString('base64');
       let obj = JSON.stringify({'screenshot': buffedScreen, 'html': buffedHtml});
@@ -26,7 +26,16 @@ module.exports = function(app) {
     });
   });
   app.post('/crawl/crawl_site', (req,res) => {
-    console.log(req.body);
-    crawler.mainCrawl(req.body);
+    // console.log(req.body);
+    crawler.mainCrawl(req.body, (responseMessage) => {
+      if (responseMessage.length > 1) {
+        res.status(400);
+        res.contentType('text/html');
+        res.send(error);
+      } else {
+        res.contentType('text/html');
+        res.send('Information received, rover crawl started');
+      }
+    });
   });
 };
